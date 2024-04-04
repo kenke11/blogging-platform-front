6bbd9adc857571988fa69b83446ec50549727ac3
tazo kenkebashvili <kenkeba@tazos-MacBook-Pro.local>
2024-04-04 15:51:45 +0400

    chore: install router

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../view/HomeView.vue";
+import LoginView from "../view/LoginView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
